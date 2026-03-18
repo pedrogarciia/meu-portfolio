e33a51a148d42d7c5687906c2ddd61ed77ab39fe
Pedro Garcia <108310232+pedrogarciia@users.noreply.github.com>
2026-03-17 21:03:21 -0300

    Update layout.tsx
    Muda o padrão do site para Jogo da forca.
    
    Mudar quando for adicionar novos projetos.
    
    (solução provisória)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Pedro Garcia",
-  description: "Portfólio",
+  title: {
+    default: "Jogo da forca | Pedro Garcia",
+    template: "%s | Pedro Garcia",
+  },
+  description: "Portfólio de desenvolvimento web",
 };
 
 export default function RootLayout({
